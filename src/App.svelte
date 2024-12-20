75fa55e3370db5c1907b0b3be61d7d408fdf6f63
reold <redicrafty@gmail.com>
2024-12-21 00:05:19 +0530

    + simple hash routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-  import Entry from "./lib/Entry.svelte";
-  import EntryInfo from "./lib/EntryInfo.svelte";
+  import Home from "./pages/Home.svelte";
+  import Dash from "./pages/Dash.svelte";
+  import type { Component } from "svelte";
 
-  let visible = $state(false);
-  let infoVisible = $state(false);
+  const routing: { [key: string]: { component: Component } } = {
+    home: { component: Home },
+    dashboard: { component: Dash },
+  };
+
+  let route = $state(routing.home);
+  let props = $state({});
+
+  const setHashRouting = () => {
+    const routeParams = window.location.hash.slice(1);
+
+    if (!routing[routeParams]) {
+      if (routeParams.startsWith("join")) {
+        props = {
+          jamInfo: { create: false, roomId: routeParams.substring(4) },
+          showJAM: true,
+        };
+        route = routing.dashboard;
+      }
+      return;
+    }
+    route = routing[routeParams];
+  };
+
+  $effect(setHashRouting);
+  window.addEventListener("hashchange", setHashRouting);
 </script>
 
-<main
-  class="w-screen h-screen bg-zinc-800 text-white overflow-y-clip flex flex-col items-center justify-center px-5"
-  style="user-select: none;"
->
-  <h1 class="text-5xl font-black">Carter</h1>
-  <p class="mt-[1ch] text-center">
-    Blazingly performant, light-weight, feature loaded music application for all
-    your needs.
-  </p>
-  <div class="flex flex-row space-x-2">
-    <button
-      class="bg-green-400 text-black rounded-md mt-[5ch]"
-      onclick={() => (visible = true)}>install</button
-    >
-    <button
-      class="bg-blue-400 text-black rounded-md mt-[5ch]"
-      onclick={() => (infoVisible = true)}>read more</button
-    >
-  </div>
-  <p class="font-mono text-xs text-center absolute bottom-1">
-    Built with Svelte5 by @reold. Performance is key.
-  </p>
-  <Entry bind:visible></Entry>
-  <EntryInfo bind:visible={infoVisible}></EntryInfo>
-</main>
+<route.component {...props} />
