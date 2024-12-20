1aa3a63dbec562c7f16c5e84647ede99729129b7
reold <redicrafty@gmail.com>
2024-12-21 00:03:26 +0530

    + Basic Entry Sheet

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -5,21 +5,39 @@
 
 <Sheet bind:visible
   >{#snippet title()}
-    Link your Account
+    <h1 class="w-full text-center place-content-center text-2xl font-black">
+      Link your Account
+    </h1>
   {/snippet}
   {#snippet body()}
-    <p>
+    <p class="text-base">
       Connect your music profiles to get your recommendations and listings.
       Supported platforms include Spotify, JioSaavn.
+      <span class="text-sm text-zinc-500">
+        <br />This feature is in development and might not work as expected
+        right now.
+      </span>
     </p>
-    <div class="flex flex-col space-y-2 px-[2ch]">
-      <input type="text" placeholder="Spotify Profile URL" />
-      <input type="text" placeholder="JioSaavn Profile URL" />
+
+    <div class="flex flex-col space-y-2 w-[95dvw] text-black mt-[10dvh]">
+      <input
+        type="text"
+        placeholder="Spotify Profile URL"
+        class="bg-white/5 ring-0 focus:outline-none rounded-md py-1 px-2 text-white"
+      />
+      <input
+        type="text"
+        placeholder="JioSaavn Profile URL"
+        class="bg-white/5 ring-0 focus:outline-none rounded-md py-1 px-2 text-white"
+      />
     </div>
+    <button class="bg-blue-400 rounded-md text-black w-[95dvw] mt-[5dvh]"
+      >continue</button
+    >
     <button
-      class="bg-blue-400 rounded-md text-white"
+      class="bg-white/5 rounded-md text-white w-[95dvw] mt-2"
       onclick={() => {
-        visible = false;
-      }}>continue</button
+        window.location.href = "#dashboard";
+      }}>skip as a guest</button
     >{/snippet}</Sheet
 >
