c7d5bd0142767324cb15bbf7ca63aaf5c929717f
reold <redicrafty@gmail.com>
2025-01-02 13:31:20 +0530

    Updates

---
+++ assets/Geet-DaT-0Fs3.js
@@ -0,0 +1,45 @@
+import{x as $,e as b,h as c,b as B,s as n,f as m,a as d,B as C,t as h,d as G,$ as E,g as s,y as F,E as x,c as p}from"./index-Bx8qNm0Q.js";import{h as R,i as S}from"./if-CHcy23qg.js";import{e as T,i as Y,b as j}from"./ssh-CefOhAKZ.js";var K=h('<textarea placeholder="enter raw lyrics to start with"></textarea> <button class="bg-white/10 text-blue-400">continue</button>',1),P=h('<li class="ring-1 ring-black"><span> </span> </li>'),V=h('<h1 class="pl-2 bg-purple-500 font-black">Geet Builder(Standalone)</h1> <!> <div class="flex flex-col space-y-1 items-center bg-white/10 text-white"><ol class="max-h-[90dvh] overflow-scroll relative"><!> <div class="sticky bg-gradient-to-b from-black/0 to-black/90 w-full h-[10dvh] bottom-0 left-0"></div></ol></div>',1);function A(g,w){$(w,!0),b(c({prefix:"kpZdEhCT04KPwdsvlctTCYY6w317Vr9j",target:"000000FF00000000000000000000000000000000000000000000000000000000"}));let v=b(c([])),l=c({rawLyrics:`മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ
+
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ..... നീ എന്‍ മടി മേലെ 
+അമ്പിളീ നിന്നെ പുല്‍കി അംബരം പൂകി ഞാന്‍ മേഘമായ്‌ (2)
+നിറസന്ധ്യയായ്‌ ഞാന്‍ ആരോമലേ 
+വിടര്‍ന്നെന്നില്‍ നീ ഒരു പൊന്‍താരമായ്‌ 
+ഉറങ്ങൂ... കനവു കണ്ടുണരാനായ്‌ ഉഷസണയുമ്പോള്‍ 
+
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+ആരിരോ.. ആരിരാരാരോ 
+
+എന്റെ മടിയെന്നും നിന്റെപൂമഞ്ചം 
+എന്‍മനമെന്നും നിന്‍ പൂങ്കാവനം (2)
+ഈ ജന്മത്തിലും വരും ജന്മത്തിലും 
+ഇനി എന്‍ ജീവന്‍ താരാട്ടായ്‌ ഒഴുകേണമേ 
+മധുകണം പോലെ മഞ്ഞിന്‍മണി പോലെ 
+മയങ്ങൂ... നീ ഈ ലത മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+ആരിരോ.. ആരിരാരാരോ 
+ആരിരോ.. ആരിരാരാരോ
+
+കാലമറിയാതെ ഞാന്‍ അമ്മയായ്‌ 
+കഥയറിയാതെ നീ പ്രതിഛായയായ് (2)
+നിന്‍മനമെന്‍ ധനം നിന്‍സുഖമെന്‍ സുഖം 
+ഇനി ഈ വീണ നിന്‍ രാഗമണിമാളിക 
+മധുസ്വരം പോലെ 
+മണിസ്വനം പോലെ 
+മയങ്ങൂ... ഗാന കുടം മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+അമ്പിളീ നിന്നെ പുല്‍കി അംബരം പൂകി ഞാന്‍ മേഘമായ്‌ (2)
+നിറസന്ധ്യയായ്‌ ഞാന്‍ ആരോമലേ 
+വിടര്‍ന്നെന്നില്‍ നീ ഒരു പൊന്‍താരമായ്‌ 
+ഉറങ്ങൂ... കനവു കണ്ടുണരാനായ്‌ ഉഷസണയുമ്പോള്‍ 
+മലര്‍കൊടി പോലെ വര്‍ണത്തുടി പോലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ 
+മയങ്ങൂ.... നീ എന്‍ മടി മേലെ
+ആരിരോ.. ആരിരാരാരോ 
+ആരിരോ..... ആരിരാരാരോ`,parseRawLyrics:()=>{B(v,c(l.rawLyrics.split(`
+`).map(t=>({content:t.trim(),t:-1}))))}});var f=V();R(t=>{E.title="Geet Builder - Carter"});var u=n(m(f),2);{var _=t=>{var a=K(),e=m(a),i=n(e,2);i.__click=function(...r){var o;(o=l.parseRawLyrics)==null||o.apply(this,r)},j(e,()=>l.rawLyrics,r=>l.rawLyrics=r),d(t,a)};S(u,t=>{s(v).length==0&&t(_)})}var y=n(u,2),k=p(y),L=p(k);T(L,17,()=>s(v),Y,(t,a)=>{var e=P(),i=p(e),r=p(i),o=n(i,1,!0);F(()=>{x(r,s(a).t!=-1?s(a).t:""),x(o,s(a).content)}),d(t,e)}),d(g,f),C()}G(["click"]);export{A as default};
