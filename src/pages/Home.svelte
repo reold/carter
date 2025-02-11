2ea4999815aa41c3fc7e26fc39fb6d6d63554d69
reold <redicrafty@gmail.com>
2025-02-11 23:59:14 +0530

    / theme color

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -7,22 +7,26 @@
 </script>
 
 <svelte:head>
-  <meta property="theme-color" content="#7e22ce" />
-  <title>Carter: Get Started</title>
+  <meta
+    name="theme-color"
+    content={visible || infoVisible ? "#4d0a26" : "#be185d"}
+  />
+
+  <title>Get Started - Carter</title>
 </svelte:head>
 <main
-  class="w-screen h-[100dvh] bg-gradient-to-b from-purple-700 via-pink-700 to-purple-950 text-white overflow-y-clip flex flex-col items-center justify-center px-5"
+  class="w-screen h-[100dvh] bg-gradient-to-b from-pink-700 to-purple-700 text-white overflow-y-clip flex flex-col items-center justify-center px-5"
 >
   <h1
-    class="text-[5em] font-black bg-clip-text bg-gradient-to-b from-black to-pink-700 text-transparent"
+    class="text-[5em] font-black bg-clip-text bg-gradient-to-b from-violet-200 from-[60%] to-[90%] to-black text-transparent"
   >
     Carter.
   </h1>
-  <p class="mt-[0ch] text-center">
-    Blazingly performant, light-weight, feature loaded music application for all
-    your needs.
+  <p class="-mt-[1ch] text-center">
+    Blazingly performant, light-weight and feature loaded music application for
+    all your needs.
   </p>
-  <div class="flex flex-row space-x-2">
+  <div class="flex flex-row space-x-2 mt-[1ch]">
     <button
       class="bg-green-400 text-black rounded-md mt-[5ch] font-semibold"
       onclick={() => (visible = true)}>install</button
@@ -32,7 +36,7 @@
       onclick={() => (infoVisible = true)}>read more</button
     >
   </div>
-  <p class="font-mono text-xs text-center absolute bottom-1 text-zinc-500">
+  <p class="font-mono text-xs text-center absolute bottom-1 text-zinc-100">
     Built with Svelte5 by @reold. Performance is key.
   </p>
   <Entry bind:visible></Entry>
