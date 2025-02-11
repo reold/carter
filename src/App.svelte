77c64cd491dfc383c8b68c9d94f12bc74f37b147
reold <redicrafty@gmail.com>
2025-02-12 00:01:26 +0530

    + dynamically loading pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  import Home from "./pages/Home.svelte";
-  import Dash from "./pages/Dash.svelte";
-  import type { Component } from "svelte";
+  import { fade } from "svelte/transition";
+  import CarterLogo from "./lib/CarterLogo.svelte";
 
-  const routing: { [key: string]: { component: Component } } = {
-    home: { component: Home },
-    dashboard: { component: Dash },
+  const routing: { [key: string]: { component: any } } = {
+    home: { component: () => import("./pages/Home.svelte") },
+    dashboard: {
+      component: () => import("./pages/Dash.svelte"),
+    },
+    geet: {
+      component: () => import("./pages/Geet.svelte"),
+    },
   };
 
   let route = $state(routing.home);
@@ -31,4 +35,32 @@
   window.addEventListener("hashchange", setHashRouting);
 </script>
 
-<route.component {...props} />
+{#await route.component()}
+  <div
+    class="h-[100dvh] flex flex-col items-center justify-center px-[2.5dvw] text-white animate-pulse"
+    transition:fade={{ duration: 150, delay: 100 }}
+  >
+    <CarterLogo class="h-[30dvh] w-[30dvh] ring-1 ring-white rounded-full" />
+    <h1 class="text-2xl font-medium text-center mt-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-[1em] mr-2 animate-spin inline"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+        />
+      </svg>loading
+    </h1>
+    <p class="text-zinc-500 mt-5 text-center text-sm max-w-[75%]">
+      Carter loads pages on-demand, so you only get what you need
+    </p>
+  </div>
+{:then PageComponent}
+  <PageComponent.default {...props} />
+{/await}
