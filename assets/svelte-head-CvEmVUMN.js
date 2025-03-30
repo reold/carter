937b0ad90d62ea2b00baaf2cb92cfbf2c49dd675
reold <redicrafty@gmail.com>
2025-03-30 15:00:39 +0530

    Updates

--- assets/svelte-head-CvEmVUMN.js
+++
@@ -1,1 +0,0 @@
-import{S as t,j as o,ae as r}from"./index-C-j1b174.js";function c(e){var a;a=document.head.appendChild(t());try{o(()=>e(a),r)}finally{}}export{c as h};
---
+++ assets/svelte-head-D7DjrVBx.js
@@ -0,0 +1,1 @@
+import{S as t,j as o,ae as r}from"./index-BbD5R-65.js";function c(e){var a;a=document.head.appendChild(t());try{o(()=>e(a),r)}finally{}}export{c as h};
