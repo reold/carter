c315b96a83526ebbc512026fbc5a7f78d677a17b
reold <redicrafty@gmail.com>
2025-04-02 20:14:50 +0530

    Updates

--- assets/if-CWp80TNK.js
+++
@@ -1,1 +0,0 @@
-import{j as N,N as p,a0 as c,k as i,a1 as u,aq as E}from"./index-DDGq5mrb.js";function T(o,_,b=!1){var n=o,a=null,e=null,f=E,h=b?p:0,s=!1;const v=(r,l=!0)=>{s=!0,t(l,r)},t=(r,l)=>{f!==(f=r)&&(f?(a?c(a):l&&(a=i(()=>l(n))),e&&u(e,()=>{e=null})):(e?c(e):l&&(e=i(()=>l(n))),a&&u(a,()=>{a=null})))};N(()=>{s=!1,_(v),s||t(null,null)},h)}export{T as i};
