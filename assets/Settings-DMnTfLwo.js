8fb8d48df2953de43263bd553d3a9939a6006c23
reold <redicrafty@gmail.com>
2025-04-08 01:11:49 +0400

    Updates

---
+++ assets/Settings-DMnTfLwo.js
@@ -0,0 +1,1 @@
+import{au as a,$ as t}from"./index-DS3OMyzr.js";import{h as e}from"./svelte-head-Dz9hnQiJ.js";a();function m(o){e(r=>{t.title="Settings - Carter"})}export{m as default};
--- assets/Settings-Db8CV8LF.js
+++
@@ -1,1 +0,0 @@
-import{au as a}from"./index-DjkWYHep.js";a();function t(e){}export{t as default};
