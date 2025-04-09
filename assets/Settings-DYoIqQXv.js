cd9ec6513025371e804c3823fc1d04da201b8148
reold <redicrafty@gmail.com>
2025-04-09 17:46:36 +0400

    Updates

--- assets/Settings-DYoIqQXv.js
+++
@@ -1,1 +0,0 @@
-import{au as a,$ as t}from"./index-Dqyo6eKO.js";import{h as e}from"./svelte-head-D9Yjea8c.js";a();function m(o){e(r=>{t.title="Settings - Carter"})}export{m as default};
---
+++ assets/svelte-head-C91_P9fs.js
@@ -0,0 +1,1 @@
+import{Y as t,j as o,aj as r}from"./index-CiWLmdvW.js";function c(e){var a;a=document.head.appendChild(t());try{o(()=>e(a),r)}finally{}}export{c as h};
