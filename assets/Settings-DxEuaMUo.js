3870d069187950e4e3a77dec12574e2db21882f8
reold <redicrafty@gmail.com>
2025-04-14 17:47:16 +0400

    Updates

--- assets/Settings-DxEuaMUo.js
+++
@@ -1,1 +0,0 @@
-import{$ as t}from"./index-BLFzM5nk.js";import"./Dash-BuLKo46W.js";import{h as o}from"./svelte-head-DearH8KI.js";import"./if-DWNlLLN6.js";import"./ssh-CJCKnxI8.js";import"./Sheet-C0NTzl5i.js";import"./input-BWC399LL.js";import"./services.svelte-CutHPgy5.js";function f(r){o(i=>{t.title="Settings - Carter"})}export{f as default};
