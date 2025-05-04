215cf6695dc514b768607ff3b48c643a6f37f516
reold <redicrafty@gmail.com>
2025-05-04 20:36:18 +0400

    + vite pwa

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/carter/dev-sw.js?dev-sw', { scope: '/carter/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/carter",
-  plugins: [svelte()],
+  base: "/carter/", // ensure trailing slash for correct asset resolution on GH Pages
+  plugins: [
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: "Svara",
+        short_name: "Svara",
+        description: "Advanced mobile music player",
+        theme_color: "#000000",
+        start_url: "/carter#dashboard",
+        scope: "/carter/",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf}"],
+      },
+      registerType: "autoUpdate",
+      strategies: "generateSW",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
