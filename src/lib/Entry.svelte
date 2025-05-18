cc2b60444687db1a2c418c653cbd937aa80fe864
reold <redicrafty@gmail.com>
2025-05-18 17:17:54 +0400

    / improve content

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -8,26 +8,26 @@
     <h1
       class="w-full text-center place-content-center text-2xl font-black text-black dark:text-white"
     >
-      Link your Account
+      Install Svara
     </h1>
   {/snippet}
   {#snippet body()}
     <p class="text-base text-black dark:text-white">
       Connect your music profiles to get your recommendations and listings.
-      Supported platforms include Spotify, JioSaavn.
+
       <span class="text-sm text-black/80 dark:text-white/50">
-        <br />This feature is in development and might not work as expected
-        right now.
+        <br />Supported platforms include Spotify, JioSaavn. This feature is in
+        development and might not work as expected right now.
       </span>
     </p>
 
-    <div
+    <!-- <div
       class="flex flex-col space-y-2 w-[95dvw] text-white dark:text-black mt-[10dvh]"
     >
       <input
         type="text"
         placeholder="Spotify Profile URL"
-        class="bg-black/10 dark:bg-white/10 ring-0 focus:outline-none rounded-md py-1 px-2 text-black dark:text-white"
+        class="bg-black/10 dark:bg-white/10 ring-0 focus:outline-hidden rounded-md py-1 px-2 text-black dark:text-white"
       />
       <input
         type="text"
@@ -42,7 +42,14 @@
     <button
       class="bg-black/10 dark:bg-white/10 rounded-md text-black dark:text-white w-[95dvw] mt-2"
       onclick={() => {
-        window.location.href = "#dashboard";
+        window.location.href = "#dash";
       }}>skip as a guest</button
-    >{/snippet}</Sheet
+    > -->
+    <button
+      class="bg-black/10 dark:bg-white/10 rounded-md text-black dark:text-white w-[95dvw] mt-2"
+      onclick={() => {
+        window.location.href = "#dash";
+      }}>continue as a guest</button
+    >
+  {/snippet}</Sheet
 >
