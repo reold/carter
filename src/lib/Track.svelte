86617de573a0c4330a34e5107692bcb676a68f5f
reold <redicrafty@gmail.com>
2025-05-18 17:14:11 +0400

    / show svg if no track image given

--- src/lib/Track.svelte
+++ src/lib/Track.svelte
@@ -77,15 +77,32 @@
       }
     }}
   >
+    {#if track.image}
     <img
       src={track.image}
       alt="cover art"
       class="max-h-[7dvh]"
       loading="lazy"
     />
+    {:else}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-full stroke-violet-500"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+        />
+      </svg>
+    {/if}
   </button>
   <button
-    class="flex flex-col overflow-x-hidden -space-y-1 justify-center w-full text-left p-0"
+    class="flex flex-col overflow-x-hidden -space-y-1 justify-center text-left w-full"
   >
     <h2
       class="font-medium text-base max-w-full truncate {playing
